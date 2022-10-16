ec6dd63e0c01a9f356560cc1df40d58f024a81f4
Sid3010 <114800007+Sid3010@users.noreply.github.com>
2022-10-16 09:24:05 +0530

    Python

---
+++ 2practiceapples.py
@@ -0,0 +1,8 @@
+#harry potter has got n number of apples harry has some stuets among who he wants to 
+#distribute the apples .These n number of apples are bought by harry by his friend
+# #and he can req for few more or few less apples.u nee to print wheater a person 
+# in range is divisor of nx or not
+
+#TAKE INPUT N,M,MX FROM THE USE
+# #
+#PRINT WEATER THE NUMBERS 
