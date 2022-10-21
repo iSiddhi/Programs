d2cf28d75d89efc18fa49dd8389b7eaf98950573
Sid3010 <114800007+Sid3010@users.noreply.github.com>
2022-10-21 22:32:36 +0530

    basic programms

---
+++ 1practiceproblem.py
@@ -0,0 +1,14 @@
+#in a class teacher wants to rearrange the stuents seating positions.
+#  wants to arrange it in such a waythat each bench has two members,and their names mus
+#
+x=int(input("enter the first number"))
+y=int(input("enter the second number"))
+
+if x>y:
+    print("siddhi is a good girl")
+else:
+    print("shruti is a good girl")    
+
+    
+
+
