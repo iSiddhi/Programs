d751e46d3167c878f8f5d5ba129ebaf23d3e18eb
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2022-12-02 22:05:41 +0530

    Add files via upload

---
+++ 8stippingg.py
@@ -0,0 +1,10 @@
+'''name = input("enter your name")
+print ( name.lstrip(),"hello")
+print('HELLO',name.rstrip(),"love")
+print('HELLO',name.strip(),"sir")'''
+
+
+f_name = input("first name: ")
+l_name = input("last name: ")
+full_name = f_name.strip() + " " + l_name.strip()
+print("Hello", full_name)
