f0efc8ca40c0aaa209731cd365bdaf4b1b3df425
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2022-12-17 21:14:24 +0530

    Add files via upload

---
+++ 2stringconcatenation.py
@@ -0,0 +1,10 @@
+#strings
+#collection of characters inside single quotes or double quotes
+
+
+first_name = ("harshita")
+last_name = ("siddhi")
+
+full_name = first_name+ last_name
+
+print(full_name)
---
+++ 3.findreplace.py
@@ -0,0 +1,19 @@
+#replace
+#find
+
+
+string= " she is beutiful and she is intelligent"
+print(string.replace("she ","he"))
+
+
+
+
+
+
+
+
+
+
+
+
+
