9e21d8b0d95dc1b152d12b9c4d9295770471d508
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2022-12-21 20:08:48 +0530

    Add files via upload

---
+++ polymorphism1.py
@@ -0,0 +1,16 @@
+class React:
+    def calculateArea(self,lenght,breadth):
+        return lenght*breadth
+
+    def calculateArea(self,length):
+        return length*length  
+
+
+
+
+def add(x, y, z = 0):
+    return x + y+z
+ 
+# Driver code
+print(add(2, 3))
+print(add(2, 3, 4))    
